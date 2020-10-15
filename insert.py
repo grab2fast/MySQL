d02f688c7c21f6d0a1214c02ed088bc2d7beeb63
badal74 <72907890+badal74@users.noreply.github.com>
2020-10-15 12:02:12 +0530

    Create insert.py
    Insertion of data into MySQL

---
+++ insert.py
@@ -0,0 +1,7 @@
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
