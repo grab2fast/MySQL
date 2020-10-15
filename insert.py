04a0c189f3df615169d6ec89b7d1fe1a9e3a3230
badal74 <72907890+badal74@users.noreply.github.com>
2020-10-15 12:06:06 +0530

    Update insert.py
    Added database connection and creation of table

--- insert.py
+++ insert.py
@@ -1,5 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host = "localhost",                  # Host address
+  database = "database",               # Database name
+  user = "root",                       # Username
+  password = "password"                # Password
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("Create Table") # Table Name
+
 sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
 val = ("John", "Highway 21")
+
 mycursor.execute(sql, val)
 
 mydb.commit()
